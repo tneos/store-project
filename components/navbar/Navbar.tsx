95198be43199bf03c468678e6616c0d6f77e1524
Thomas Neos <tneos@hotmail.com>
2025-03-06 22:04:31 +0000

    Add Container and Navbar components

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,4 +1,23 @@
+import Container from "../global/Container";
+import CartButton from "./CartButton";
+import DarkMode from "./DarkMode";
+import LinksDropdown from "./LinksDropdown";
+import Logo from "./Logo";
+import NavSearch from "./NavSearch";
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="border-b">
+      <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4 flex-wrap py-8">
+        <Logo />
+        <NavSearch />
+        <div className="flex gap-4 items-center">
+          <CartButton />
+          <DarkMode />
+          <LinksDropdown />
+        </div>
+      </Container>
+    </nav>
+  );
 }
 export default Navbar;
