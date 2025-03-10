f5962ed6b2f4d18ef547fd399c12721a219cd726
Thomas Neos <tneos@hotmail.com>
2025-03-10 07:54:57 +0000

    Add navSearch and cartButton components

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,4 +1,25 @@
+import Link from "next/link";
+import {Button} from "../ui/button";
+import {LuShoppingCart} from "react-icons/lu";
+
 function CartButton() {
-  return <div>Cart Button</div>;
+  // temp
+  const numItemsInCart = 3;
+
+  return (
+    <Button
+      asChild
+      variant="outline"
+      size="icon"
+      className="flex justify-center items-center relative"
+    >
+      <Link href="/cart">
+        <LuShoppingCart />
+        <span className="absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-sm">
+          {numItemsInCart}
+        </span>
+      </Link>
+    </Button>
+  );
 }
 export default CartButton;
--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,4 +1,6 @@
+import {Input} from "../ui/input";
+
 function NavSearch() {
-  return <div>NavSearch</div>;
+  return <Input type="search" placeholder="search product" className="max-w-xs dark:bg-muted" />;
 }
 export default NavSearch;
