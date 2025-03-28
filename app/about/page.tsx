2b46685051e13608fa41976205f41a8ab8ad9548
Thomas Neos <tneos@hotmail.com>
2025-03-28 18:28:46 +0000

    Set up About page and LoadingContainer

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,16 @@
 function AboutPage() {
-  return <div>About Page</div>;
+  return (
+    <section>
+      <h1 className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-wide sm:text-6xl">
+        We love
+        <span className="bg-primary py-2 px-4 rounded-lg tracking-widest text-white">store</span>
+      </h1>
+      <p className="mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto text-muted-foreground">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora cupiditate quam aut
+        mollitia harum ipsa veritatis iusto voluptatum quo sint, temporibus nulla, autem, voluptas
+        similique praesentium! Quaerat cum quas a?
+      </p>
+    </section>
+  );
 }
 export default AboutPage;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
+import LoadingContainer from "@/components/global/LoadingContainer";
 import FeaturedProducts from "@/components/home/FeaturedProducts";
 import Hero from "@/components/home/Hero";
+import {Suspense} from "react";
 
 const HomePage = () => {
   return (
     <>
       <Hero />
-      <FeaturedProducts />
+      <Suspense fallback={<LoadingContainer />}>
+        <FeaturedProducts />
+      </Suspense>
     </>
   );
 };
