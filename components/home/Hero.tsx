39919ed4ded9ff9c53381c8dd15906c980f77228
Thomas Neos <tneos@hotmail.com>
2025-03-28 17:12:17 +0000

    Set up Hero and HeroCarousel components

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,4 +1,25 @@
+import Link from "next/link";
+import {Button} from "../ui/button";
+import HeroCarousel from "./HeroCarousel";
+
 function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-24 items-center">
+      <div>
+        <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6x">
+          We are changing the way people shop..
+        </h1>
+        <p className="mt-8 max-w-xl text-lg leading-8 text-muted-foreground">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, assumenda a maiores
+          accusantium aperiam possimus ea eius iste illum iusto magnam fugit quisquam! Cum,
+          numquam. Error temporibus aspernatur iste assumenda?
+        </p>
+        <Button asChild size="lg" className="mt-10">
+          <Link href="/products">Our Products</Link>
+        </Button>
+      </div>
+      <HeroCarousel />
+    </section>
+  );
 }
 export default Hero;
