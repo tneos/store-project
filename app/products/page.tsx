fe59554194783703c776fd9e10c8feac3b7667ae
Thomas Neos <tneos@hotmail.com>
2025-03-29 13:18:47 +0000

    Set grid and list layout and add ProductsList component

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,4 +1,11 @@
-function ProductsPage() {
-  return <div>Products Page</div>;
+import ProductsContainer from "@/components/products/ProductsContainer";
+
+function ProductsPage({searchParams}: {searchParams: {layout?: string; search?: string}}) {
+  // Set default values if props undefined
+  const layout = searchParams.layout || "grid";
+  const search = searchParams.search || "";
+  console.log(searchParams);
+
+  return <ProductsContainer layout={layout} search={search} />;
 }
 export default ProductsPage;
