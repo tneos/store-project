69219aad3355487b647c6ae5452603dc300548a6
Thomas Neos <tneos@hotmail.com>
2025-04-12 16:25:53 +0100

    Setup admin links and structure for admin components

---
+++ app/admin/Sidebar.tsx
@@ -0,0 +1,26 @@
+"use client";
+import {adminLinks} from "@/utils/links";
+import Link from "next/link";
+import {usePathname} from "next/navigation";
+import {Button} from "@/components/ui/button";
+
+function Sidebar() {
+  const pathname = usePathname();
+
+  return (
+    <aside>
+      {adminLinks.map(link => {
+        const isActivePage = pathname === link.href;
+        const variant = isActivePage ? "default" : "ghost";
+        return (
+          <Button asChild className="w-full mb-2 capitalize font-normal" variant={variant}>
+            <Link key={link.href} href={link.href}>
+              {link.label}
+            </Link>
+          </Button>
+        );
+      })}
+    </aside>
+  );
+}
+export default Sidebar;
