62ce903cc93a88baa931ebf78438365880eff478
Thomas Neos <tneos@hotmail.com>
2025-04-17 08:16:27 +0100

    Add create product action, initial commit(hardcoded image)

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,7 +1,19 @@
 "use server";
 
 import db from "@/utils/db";
+import {currentUser} from "@clerk/nextjs/server";
 import {redirect} from "next/navigation";
+
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) redirect("/");
+  return user;
+};
+
+const renderError = (error: unknown): {message: string} => {
+  console.log(error);
+  return {message: error instanceof Error ? error.message : "an error occurred"};
+};
 
 export const fetchFeaturedProducts = async () => {
   const products = await db.product.findMany({
@@ -41,5 +53,30 @@ export const createProductAction = async (
   prevState: any,
   formData: FormData
 ): Promise<{message: string}> => {
-  return {message: "product created"};
+  const user = await getAuthUser();
+  try {
+    const name = formData.get("name") as string;
+    const company = formData.get("company") as string;
+    const price = Number(formData.get("price") as string);
+    // temp
+    const image = formData.get("image") as File;
+    const description = formData.get("description") as string;
+    const featured = Boolean(formData.get("featured") as string);
+
+    await db.product.create({
+      data: {
+        name,
+        company,
+        price,
+        image: "/images/product-1.jpg",
+        description,
+        featured,
+        clerkId: user.id,
+      },
+    });
+
+    return {message: "product created"};
+  } catch (error) {
+    return renderError(error);
+  }
 };
