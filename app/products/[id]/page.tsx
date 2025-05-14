abc36725a3993225715e457ac46dec81749f1bc5
Thomas Neos <tneos@hotmail.com>
2025-05-14 18:35:11 +0100

    Setup SubmitReview and RatingInput components

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -6,6 +6,8 @@ import FavoriteToggleButton from "@/components/products/FavoriteToggleButton";
 import AddToCart from "@/components/single-product/AddToCart";
 import ProductRating from "@/components/single-product/ProductRating";
 import ShareButton from "@/components/single-product/ShareButton";
+import SubmitReview from "@/components/reviews/SubmitReview";
+import ProductReviews from "@/components/reviews/ProductReviews";
 
 async function page({params}: {params: {id: string}}) {
   // Await params -- Next 15
@@ -47,6 +49,8 @@ async function page({params}: {params: {id: string}}) {
           <AddToCart productId={productId} />
         </div>
       </div>
+      <ProductReviews productId={params.id} />
+      <SubmitReview productId={params.id} />
     </section>
   );
 }
