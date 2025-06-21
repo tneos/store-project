9af8f4ee57985dcb0f35a4d1a5a378b78e970976
Thomas Neos <tneos@hotmail.com>
2025-06-21 16:14:05 +0100

    Refactor order Model and refactor create order action

--- utils/actions.ts
+++ utils/actions.ts
@@ -541,8 +541,19 @@ export const updateCartItemAction = async ({
 
 export const createOrderAction = async (prevState: any, formData: FormData) => {
   const user = await getAuthUser();
+  let orderId: null | string = null;
+  let cartId: null | string = null;
+
   try {
     const cart = await fetchOrCreateCart({userId: user.id, errorOnFailure: true});
+    cartId = cart.id;
+
+    await db.order.deleteMany({
+      where: {
+        clerkId: user.id,
+        isPaid: false,
+      },
+    });
     const order = await db.order.create({
       data: {
         clerkId: user.id,
@@ -553,16 +564,12 @@ export const createOrderAction = async (prevState: any, formData: FormData) => {
         email: user.emailAddresses[0].emailAddress,
       },
     });
-    // Remove existing cart when order proceeds successfully
-    await db.cart.delete({
-      where: {
-        id: cart.id,
-      },
-    });
+    orderId = order.id;
   } catch (error) {
     return renderError(error);
   }
-  redirect("/orders");
+  // Redirect to checkout page(params added)
+  redirect(`/checkout?orderId=${orderId}&cartId=${cartId}`);
 };
 
 export const fetchUserOrders = async () => {
