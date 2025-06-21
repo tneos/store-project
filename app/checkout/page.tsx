6f8c00b04a9b10994653c479e35c1ca87b2af2a5
Thomas Neos <tneos@hotmail.com>
2025-06-21 18:08:00 +0100

    Add checkout page make request to server and setup stripe

---
+++ app/checkout/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+import axios from "axios";
+import {useSearchParams} from "next/navigation";
+import React, {useCallback} from "react";
+import {loadStripe} from "@stripe/stripe-js";
+import {EmbeddedCheckoutProvider, EmbeddedCheckout} from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe(process.env.NEXT_STRIPE_PUBLISHABLE_KEY as string);
+
+function CheckoutPage() {
+  // Get params
+  const searchParams = useSearchParams();
+  const orderId = searchParams.get("orderId");
+  const cartId = searchParams.get("cartId");
+
+  // Make request to server
+  const fetchClientSecret = useCallback(async () => {
+    const response = await axios.post("/api/payment", {
+      orderId,
+      cartId,
+    });
+
+    // Return stripe key to client
+    return response.data.clientSecret;
+  }, []);
+
+  const options = {fetchClientSecret};
+
+  return (
+    <div id="checkout">
+      <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
+        <EmbeddedCheckout />
+      </EmbeddedCheckoutProvider>
+    </div>
+  );
+}
+export default CheckoutPage;
