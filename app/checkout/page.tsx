1f948179d45013dedb4a85cdf35540400300548d
Thomas Neos <tneos@hotmail.com>
2025-06-26 21:57:53 +0100

    Update variable

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -5,7 +5,7 @@ import React, {useCallback} from "react";
 import {loadStripe} from "@stripe/stripe-js";
 import {EmbeddedCheckoutProvider, EmbeddedCheckout} from "@stripe/react-stripe-js";
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string);
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE as string);
 
 export default function CheckoutPage() {
   // Get params
